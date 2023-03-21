47277a0f60fde12009fc81d87cfb752fec38788f
ConnorT-72 <127335221+ConnorT-72@users.noreply.github.com>
2023-03-21 21:53:28 +0000

    Add project files.

---
+++ AlgorithmsAndComplexityAssignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlgorithmsAndComplexityAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"\RoadFiles");
+            for (int i = 0; i < lines.Length; i++)
+                Console.WriteLine(lines[i]);
+        }
+    }
+}
